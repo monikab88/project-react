c9a4d370a8255a64db09dacc7702a0ea477f33e8
Monika Bielicz <bieliczm@gmail.com>
2023-07-20 03:24:56 +0200

    components ColumnForm title icon

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -14,7 +14,7 @@ const List = () => {
   ]);
   
   const addColumn = newColumn => {
-		setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+		setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
   };
  
     
