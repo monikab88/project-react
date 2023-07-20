527ee74352aafeeff19a1d3709cefcbd8e612055
Monika Bielicz <bieliczm@gmail.com>
2023-07-20 02:36:21 +0200

    handleSubmit

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,7 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import { useState } from 'react';
-import  {useEffect} from 'react';
+import shortid from 'shortid';
 
 const List = () => {
   
@@ -11,13 +11,14 @@ const List = () => {
     { id: 3, title: 'Games', icon: 'gamepad' }
   ]);
   
-  useEffect(() => {
-
-    setTimeout(() => {
-        setColumns([...columns, { id: 4, title: 'Test column'}]);
-    }, 2000);
-
-}, []);
+  const [value, setValue] = useState('');
+ 
+  const handleSubmit = e => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  }; 
+ 
     
     return (
       <div className={styles.list}>
@@ -28,6 +29,10 @@ const List = () => {
           <section className={styles.columns}>
             {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
           </section>
+          <form onSubmit={handleSubmit}>
+           <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+           <button>Add column</button>
+          </form>
         </div> 
     );
   };
