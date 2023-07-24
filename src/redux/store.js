a54d703bd527fdfc6f891f5f878475b16ea50c95
Monika Bielicz <bieliczm@gmail.com>
2023-07-24 21:18:14 +0200

    Redux components ColumnForm ADD_COLUMN

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
 
+
 const reducer = (state, action) => {
+  if(action.type === 'ADD_COLUMN') return { ...state, columns: [...state.columns, action.newColumn]}
   return state;
 };
 
