fb58b4987ae2315892b04927c99bf0714fdcc70a
Monika Bielicz <bieliczm@gmail.com>
2023-07-25 00:52:06 +0200

    Redux components SearchForm UPDATE_SEARCHSTRING

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,23 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+
+    const dispatch = useDispatch();
+        
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload:  title});
+    }
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput placeholder="Search..." value={title} onChange={e => setTitle(e.target.value)} />
             <Button>
                 <span className="fa fa-search" />
             </Button>
