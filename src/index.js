78a88fd63981ca41297b640831de6f3df9127899
Monika Bielicz <bieliczm@gmail.com>
2023-07-24 18:50:39 +0200

    install Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/normalize.scss';
-import './styles/global.scss';
-import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <App />,
