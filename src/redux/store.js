fc5d59a473b4b7042039d3b025cfceb33c267f9f
Monika Bielicz <bieliczm@gmail.com>
2023-07-26 00:52:13 +0200

    Redux components ListForm ADD_LIST

--- src/redux/store.js
+++ src/redux/store.js
@@ -23,6 +23,8 @@ export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 
+export const addList = payload => ({ type: 'ADD_LIST', payload });
+
 export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 
 const reducer = (state, action) => {
@@ -31,6 +33,8 @@ const reducer = (state, action) => {
           return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }]};
         case 'ADD_CARD':
             return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }]};
+        case 'ADD_LIST':
+            return { ...state, lists: [...state.lists, { ...action.payload, id: shortid() }]};
         case 'UPDATE_SEARCHSTRING':
             return { ...state, searchString: action.payload};
         default:
