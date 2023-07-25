821022af54618b14f6346e175b10701b542e0322
Monika Bielicz <bieliczm@gmail.com>
2023-07-25 03:11:58 +0200

    Redux action creator updateSearchString

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,6 +14,8 @@ export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 
+export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
+
 const reducer = (state, action) => {
     switch(action.type) {
         case 'ADD_COLUMN':
