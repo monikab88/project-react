935dc2c495467b6948e971aa011622f2406b2396
Monika Bielicz <bieliczm@gmail.com>
2023-07-25 17:05:27 +0200

    change NavBar component Link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import styles from './NavBar.module.scss';
 import Container from '../Container/Container';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -10,11 +11,11 @@ const NavBar = () => {
             <a className={styles.icon} href="/"><i className="fa fa-tasks" /></a>
           </div>
           <div className={styles.col}>
-            <ul className={styles.row}>
-              <li><a href="/">Home</a></li>
-              <li><a href="/favorite">Favorite</a></li>
-              <li><a href="/about">About</a></li>
-            </ul>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/favorite">Favorite</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
           </div>
         </div>
         </Container>
