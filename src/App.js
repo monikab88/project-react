22249a08519cc7152595a5f74adf13e8a6675aff
Monika Bielicz <bieliczm@gmail.com>
2023-07-25 16:05:03 +0200

    components Route/Routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Container from './components/Container/Container.js';
 import NavBar from './components/NavBar/NavBar.js';
 import Favorite from './components/Favorite/Favorite.js';
 import About from './components/About/About.js';
+import { Routes, Route } from 'react-router-dom';
 
 
 const App = () => {
   return (
     <main>
-     <NavBar />
-       <Container>
-         <Home />
-         <About />
-         <Favorite />
-       </Container>
-    </main>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </Container>
+ </main>
   );
 };
 
