b673d2515cc95e4ef98cc92928b50416b0953692
Monika Bielicz <bieliczm@gmail.com>
2023-07-26 00:34:55 +0200

    change List component Navigate

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,16 +5,18 @@ import SearchForm from '../SearchForm/SearchForm';
 import { useSelector } from 'react-redux';
 import { getListById, getColumnsByList } from '../../redux/store';
 import { useParams } from 'react-router';
+import { Navigate } from 'react-router';
 
 
-const List = props => {
+const List = () => {
 
   const { listId } = useParams();
   const columns = useSelector((state) => getColumnsByList(state, listId));
   const listData = useSelector((state) => getListById(state, listId));
   
   
-    return (
+  if(!listData) return <Navigate to="/" />
+    return(
       <div className={styles.list}>
         <header className={styles.header}>
           <h2 className={styles.title}>{listData.title}</h2>
