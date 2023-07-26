491dfe6ee6e8d2c026e7e5a5480283e280f75a08
Monika Bielicz <bieliczm@gmail.com>
2023-07-26 04:18:25 +0200

    Redux components Card REMOVE_CARD

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,36 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card  = props => {
 
   const dispatch = useDispatch();
 
-  const handleClick = e => {
+  const toggleFavorite = e => {
     e.preventDefault();
     dispatch(toggleCardFavorite(props.id))
+  };
+
+  const handleClick = e => {
+    e.preventDefault();
+    dispatch(removeCard(props.id))
   };
   
     return (
       <li className={styles.card}>
+        <div>
         {props.title}
-        <button onClick={handleClick} className={clsx(styles.button, props.isFavorite && styles.active)}><i className="fa fa-star-o"></i></button>
-        </li>
+        </div>
+        <div>
+          <button onClick={toggleFavorite} className={clsx(styles.button, props.isFavorite && styles.active)}>
+            <i className="fa fa-star-o"></i>
+          </button>
+          <button onClick={handleClick} className={clsx(styles.button, props.isFavorite && styles.active)}>
+            <i className="fa fa-trash"></i>
+          </button>
+        </div>
+      </li>
     );
 };
 
